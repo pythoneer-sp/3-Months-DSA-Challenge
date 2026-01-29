95f9c73accbf4c2badfcb247f2719d31cf8d3983
Saurav Kumar <155866156+pythoneer-sp@users.noreply.github.com>
2026-01-29 22:45:35 +0530

    day 11

---
+++ Two Pointers/Day 12/ContainerWithMostWater(16).py
@@ -0,0 +1,12 @@
+def maxArea(height) -> int:
+    start = 0
+    end = len(height)-1
+    res = 0
+    while start<end:
+        area = min(height[start],height[end]) * (end-start)
+        res = max(res,area)
+        if height[start] <= height[end]:
+            start +=1
+        else:
+            end -= 1
+    return res
